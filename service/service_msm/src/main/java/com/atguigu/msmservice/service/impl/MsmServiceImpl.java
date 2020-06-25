823c28cf9ba17d42ad09b253ee6a1956f659d8f2
hayashietsu <1191609469@qq.com>
2020-06-26 02:35:51 +0800

    ly/06/26

---
+++ service/service_msm/src/main/java/com/atguigu/msmservice/service/impl/MsmServiceImpl.java
@@ -0,0 +1,49 @@
+package com.atguigu.msmservice.service.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+import com.atguigu.msmservice.service.MsmService;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
+
+@Service
+public class MsmServiceImpl implements MsmService {
+
+    //发送短信的方法
+    @Override
+    public boolean send(Map<String, Object> param, String phone) {
+        if (StringUtils.isEmpty(phone)){
+            return false;
+        }
+        DefaultProfile profile=DefaultProfile.getProfile("default","LTAI4GBjdHCF9YUgLS9cRZzA","DhiH0EOlqbqDL5v1cjcbo2xuiAqF4a");
+        IAcsClient client=new DefaultAcsClient(profile);
+//设置参数
+        CommonRequest request=new CommonRequest();
+        request.setMethod(MethodType.POST);
+        request.setDomain("dysmsapi.aliyuncs.com");
+        request.setVersion("2017-05-25");
+        request.setAction("SendSms");
+
+//设置发送相关的参数
+        request.putQueryParameter("PhoneNumbers",phone);//手机号
+        request.putQueryParameter("SignName","我的才悦酒行网页");//签名名称
+        request.putQueryParameter("TemplateCode","SMS_193518167"); //模板code
+        request.putQueryParameter("TemplateParam",JSONObject.toJSONString(param)); //验证码数据
+//发送
+        try{
+            CommonResponse response=client.getCommonResponse(request);
+            boolean success=response.getHttpResponse().isSuccess();
+            return success;
+        }catch (Exception e){
+
+        }
+        return false;
+    }
+}
